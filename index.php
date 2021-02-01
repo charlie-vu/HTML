0e195f60c92fdbbbefb983ae5fe39f848f10c67a
vanvu25894 <vanvu25894@gmail.com>
2021-02-01 09:31:29 +0700

    Create php file for Heroku

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aptech-VuNgocVan</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Vũ Ngọc Văn - Bài tập thực hành</h1>
+    </div>
+</body>
+</html>

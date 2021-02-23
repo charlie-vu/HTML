de4a8fd08f1dea06ace6d09b41dddb1655d08f4e
vanvu25894 <vanvu25894@gmail.com>
2021-02-23 16:22:50 +0700

    index-update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="_index/css/index.css">
+    <link rel="stylesheet" href="00_index/css/index.css">
     <title>Ngoc-Van Vu - Aptechltn</title>
 </head>
 <body>
